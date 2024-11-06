b6b24531d4561c85609934df561943bb8949b365
matanyam <matanyamelmaliah@gmail.com>
2024-11-06 19:50:26 +0200

    fix flask import

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, make_response, jsonify, request, render_template # type: ignore
+from flask import Flask, make_response, jsonify, request, render_template  # type: ignore
 import time
 
 app = Flask(__name__)
-    
+
+
 @app.route('/health', methods=['GET'])
 def healthcheck():
     return make_response(jsonify({'status': 'healthy'}), 200)
+
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template('main.html', title='Weather App')
 
+
 @app.route('/<filename>', methods=['GET'])
 def static_files(filename):
     return app.send_static_file(filename)
+
 
 @app.route('/get_weather', methods=['GET'])
 def get_weather():
@@ -22,7 +26,5 @@ def get_weather():
     weather = gw(city)
     return jsonify({'City': city, 'Temp': weather})
 
-app.run(debug=True, host="0.0.0.0", port="8080")
 
-    
-    
+app.run(debug=True, host="0.0.0.0", port="8080")
