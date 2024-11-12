6c370e6a3444fc6f99c3f1d80ed2a1b5be229784
ספיר אלמליח <matanyamelmaliah@gmail.com>
2024-11-12 18:51:54 +0200

    ex c done

---
+++ EX-C/static/script.js
@@ -0,0 +1,24 @@
+const Form = document.getElementById('check-form');
+const status = document.getElementById('status');
+
+Form.addEventListener('submit', function(event) {
+    console.log('Form is submitted!');
+    event.preventDefault();
+    const url = document.getElementById('url').value;
+    console.log(`URL: ${url}`);
+    checkURL(url);
+});
+
+async function checkURL(url) {
+    const response = await fetch(`/check_url?url=${url}`);
+    const data = await response.json();
+    if (data.status_code == 'INVALID') {
+    console.log('Invalid URL format')
+    alert('Invalid url format')
+    }
+
+    console.log(data);
+    console.log(data.status_code);
+    status.textContent = `Status code: ${data.status_code}`;
+
+}
